b447631937b0e00e5a5229e46351eff1feb56243
irconde <irconde@ualr.edu>
2020-08-14 11:58:04 -0500

    Added new TODO comments

--- app/src/main/java/edu/ualr/navigation/MainActivity.java
+++ app/src/main/java/edu/ualr/navigation/MainActivity.java
@@ -5,28 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
-// TODO 01. We have to create the Fragment classes that we are going to use to instantiate from the activity
+import edu.ualr.navigation.databinding.ActivityMainBinding;
+
+// TODO 05. We have to create the Fragment classes that we are going to use to instantiate from the activity
 // FormFragment and PizzaOrderFragment
-// TODO 02. Define the corresponding layout files
+// TODO 06. Define the corresponding layout files
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager2 viewPager;
+    private ActivityMainBinding mBinding;
     private FragmentStateAdapter pageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        viewPager = findViewById(R.id.pager);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        // TODO 04. Create a new instance of the FragmentStateAdapter class created and plug it into the viewpager
         pageAdapter = new ScreenSlidePagerAdapter(this);
-        viewPager.setAdapter(pageAdapter);
+        mBinding.pager.setAdapter(pageAdapter);
     }
 
+    // TODO 03. Define a new FragmentStateAdapter class that will manage the visualization of the proper fragment
     private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
 
         private static final int NUM_PAGES = 2;
@@ -38,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
             super(fragmentActivity);
         }
 
-        // TODO 03. We instantiate the proper Fragment class based on the current position of the View pager
+        // TODO 07. Override the createFragment method to instantiate the proper Fragment class based on the current position of the View pager
         @NonNull
         @Override
         public Fragment createFragment(int position) {
